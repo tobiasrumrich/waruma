f82e1d49c39b4b299dc6e61cfb857b3696a43eaf
tobias.rumrich@googlemail.com <tobias.rumrich@googlemail.com@3ea0aeef-910e-5cde-5b2f-4d9b01b7df46>
2010-11-22 10:13:32 +0000

    111

--- RushHour/src/de/nordakademie/waruma/testcase/PenisTestClass.java
+++ RushHour/src/de/nordakademie/waruma/testcase/PenisTestClass.java
@@ -3,4 +3,12 @@ package de.nordakademie.waruma.testcase;
 public class PenisTestClass {
 	private String test999;
 	private int length;
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	
+	
 }
