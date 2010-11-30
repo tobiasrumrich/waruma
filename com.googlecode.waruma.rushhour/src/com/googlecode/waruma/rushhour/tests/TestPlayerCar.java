15c0ea6b6475faeaa1d56dcf59256826368bbd35
tobias.rumrich@gmail.com <tobias.rumrich@gmail.com@3ea0aeef-910e-5cde-5b2f-4d9b01b7df46>
2010-11-30 22:12:30 +0000

    Adapted to PlayerCar

--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/tests/TestPlayerCar.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/tests/TestPlayerCar.java
@@ -3,7 +3,7 @@ package com.googlecode.waruma.rushhour.tests;
 import java.awt.Point;
 
 import com.googlecode.waruma.rushhour.framework.Orientation;
-import com.googlecode.waruma.rushhour.game.StandardCar;
+import com.googlecode.waruma.rushhour.game.PlayerCar;
 
 import junit.framework.TestCase;
 
@@ -14,13 +14,13 @@ import junit.framework.TestCase;
  */
 public class TestPlayerCar extends TestCase {
 	public Boolean[][] collisionMap;
-	private StandardCar car;
+	private PlayerCar car;
 
 	protected void setUp() throws Exception {
 		super.setUp();
 		collisionMap = new Boolean[][] { { true, true } };
 
-		car = new StandardCar(collisionMap, new Point(3, 1), Orientation.NORTH);
+		car = new PlayerCar(collisionMap, new Point(3, 1), Orientation.NORTH);
 	}
 
 	public void testPlayerCar() {
@@ -29,9 +29,73 @@ public class TestPlayerCar extends TestCase {
 		assertEquals(Orientation.NORTH,car.getOrientation());
 	}
 
-	public void testMoveInteger() {
-		fail("Not yet implemented");
+	public void testMoveWithNorth() {
+		//Wir positionieren unser Auto auf (5,5) und richten es mit unserem Kompass nach Norden aus
+		car.setOrientation(Orientation.NORTH);
+		car.setPosition(new Point(5,5));
+		
+		//Wir stehen auf (5,5) und fahren 3 Felder vorwärts
+		car.move(3);
+		//Wir sollten nun auf (5,2) stehen
+		assertEquals(new Point(5,2),car.getPosition());
+		
+		//Wir stehen auf (5,2) und fahren 6 Felder rückwärts
+		car.move(-6);
+		//Wir sollten nun auf (5,8) stehen
+		assertEquals(new Point(5,8),car.getPosition());
 	}
+	
+	public void testMoveWithSouth() {
+		//Wir positionieren unser Auto auf (5,5) und richten es mit unserem Kompass nach Süden aus
+		car.setOrientation(Orientation.SOUTH);
+		car.setPosition(new Point(5,5));
+		
+		//Wir stehen auf (5,5) und fahren 3 Felder vorwärts
+		car.move(3);
+		//Wir sollten nun auf (5,8) stehen
+		assertEquals(new Point(5,8),car.getPosition());
+		
+		//Wir stehen auf (5,8) und fahren 6 Felder rückwärts
+		car.move(-6);
+		//Wir sollten nun auf (5,2) stehen
+		assertEquals(new Point(5,2),car.getPosition());
+		
+	}
+	
+	public void testMoveWithWest() {
+		//Wir positionieren unser Auto auf (5,5) und richten es mit unserem Kompass zu den alten Bundesländern aus
+		car.setOrientation(Orientation.WEST);
+		car.setPosition(new Point(5,5));
+		
+		//Wir stehen auf (5,5) und fahren 3 Felder vorwärts
+		car.move(3);
+		//Wir sollten nun auf (2,5) stehen
+		assertEquals(new Point(2,5),car.getPosition());
+		
+		//Wir stehen auf (2,5) und fahren 6 Felder rückwärts
+		car.move(-6);
+		//Wir sollten nun auf (8,5) stehen
+		assertEquals(new Point(8,5),car.getPosition());
+		
+	}
+	
+	public void testMoveWithEast() {
+		//Wir positionieren unser Auto auf (5,5), es wurde offensichtlich gestohlen und ist Richtung Polen ausgerichtet
+		car.setOrientation(Orientation.EAST);
+		car.setPosition(new Point(5,5));
+		
+		//Wir stehen auf (5,5) und fahren 3 Felder vorwärts
+		car.move(3);
+		//Wir sollten nun auf (8,5) stehen
+		assertEquals(new Point(8,5),car.getPosition());
+		
+		//Wir stehen auf (8,5) und fahren 6 Felder rückwärts
+		car.move(-6);
+		//Wir sollten nun auf (2,5) stehen
+		assertEquals(new Point(2,5),car.getPosition());
+		
+	}
+
 
 	public void testRegisterReachedDestination() {
 		fail("Not yet implemented");
