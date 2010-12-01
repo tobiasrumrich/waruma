2198d9048a86aaab81bab569f021b002e89583ed
mail2fabi@gmail.com <mail2fabi@gmail.com@3ea0aeef-910e-5cde-5b2f-4d9b01b7df46>
2010-12-01 16:44:27 +0000

    weiteres Update vom IllegalMoveException

--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/tests/TestPlayerCar.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/tests/TestPlayerCar.java
@@ -2,6 +2,7 @@ package com.googlecode.waruma.rushhour.tests;
 
 import java.awt.Point;
 
+import com.googlecode.waruma.rushhour.exceptions.IllegalMoveException;
 import com.googlecode.waruma.rushhour.framework.Orientation;
 import com.googlecode.waruma.rushhour.game.PlayerCar;
 
@@ -15,6 +16,7 @@ import junit.framework.TestCase;
 public class TestPlayerCar extends TestCase {
 	public Boolean[][] collisionMap;
 	private PlayerCar car;
+	private boolean thrownException;
 
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -29,7 +31,7 @@ public class TestPlayerCar extends TestCase {
 		assertEquals(Orientation.NORTH,car.getOrientation());
 	}
 
-	public void testMoveWithNorth() {
+	public void testMoveWithNorth() throws IllegalMoveException{
 		//Wir positionieren unser Auto auf (5,5) und richten es mit unserem Kompass nach Norden aus
 		car.setOrientation(Orientation.NORTH);
 		car.setPosition(new Point(5,5));
@@ -45,7 +47,7 @@ public class TestPlayerCar extends TestCase {
 		assertEquals(new Point(5,8),car.getPosition());
 	}
 	
-	public void testMoveWithSouth() {
+	public void testMoveWithSouth() throws IllegalMoveException {
 		//Wir positionieren unser Auto auf (5,5) und richten es mit unserem Kompass nach Süden aus
 		car.setOrientation(Orientation.SOUTH);
 		car.setPosition(new Point(5,5));
@@ -62,7 +64,7 @@ public class TestPlayerCar extends TestCase {
 		
 	}
 	
-	public void testMoveWithWest() {
+	public void testMoveWithWest() throws IllegalMoveException {
 		//Wir positionieren unser Auto auf (5,5) und richten es mit unserem Kompass zu den alten Bundesländern aus
 		car.setOrientation(Orientation.WEST);
 		car.setPosition(new Point(5,5));
@@ -79,7 +81,7 @@ public class TestPlayerCar extends TestCase {
 		
 	}
 	
-	public void testMoveWithEast() {
+	public void testMoveWithEast() throws IllegalMoveException {
 		//Wir positionieren unser Auto auf (5,5), es wurde offensichtlich gestohlen und ist Richtung Polen ausgerichtet
 		car.setOrientation(Orientation.EAST);
 		car.setPosition(new Point(5,5));
@@ -96,6 +98,21 @@ public class TestPlayerCar extends TestCase {
 		
 	}
 
+	public void testMoveWithZero () throws IllegalMoveException {
+		thrownException = false;
+		
+		//Wir positionieren unser Auto auf (5,5) und richten es mit unserem Kompass nach Norden aus
+		car.setOrientation(Orientation.NORTH);
+		car.setPosition(new Point(5,5));
+		
+		try {
+			car.move(0);
+		}
+		catch (IllegalMoveException e) {
+			thrownException = true;
+		}
+		assertTrue(thrownException);
+	}
 
 	public void testRegisterReachedDestination() {
 		fail("Not yet implemented");
--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/tests/TestStandardCar.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/tests/TestStandardCar.java
@@ -1,7 +1,9 @@
 package com.googlecode.waruma.rushhour.tests;
 
+
 import java.awt.Point;
 
+import com.googlecode.waruma.rushhour.exceptions.IllegalMoveException;
 import com.googlecode.waruma.rushhour.framework.Orientation;
 import com.googlecode.waruma.rushhour.game.StandardCar;
 
@@ -11,6 +13,7 @@ public class TestStandardCar extends TestCase {
 
 	public Boolean[][] collisionMap;
 	private StandardCar car;
+	private boolean thrownException;
 
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -28,22 +31,84 @@ public class TestStandardCar extends TestCase {
 		assertEquals(Orientation.NORTH,car.getOrientation());
 	}
 
-	public void testMove() {
-
-		car.move(-3);
-		assertEquals(new Point(3,4), car.getPosition());
+	public void testMoveWithNorth()  throws IllegalMoveException{
+		//Wir positionieren unser Auto auf (5,5) und richten es mit unserem Kompass nach Norden aus
+		car.setOrientation(Orientation.NORTH);
+		car.setPosition(new Point(5,5));
 		
-		car.move(1);
-		assertEquals(new Point (3,3), car.getPosition());
+		//Wir stehen auf (5,5) und fahren 3 Felder vorwärts
+		car.move(3);
+		//Wir sollten nun auf (5,2) stehen
+		assertEquals(new Point(5,2),car.getPosition());
+		
+		//Wir stehen auf (5,2) und fahren 6 Felder rückwärts
+		car.move(-6);
+		//Wir sollten nun auf (5,8) stehen
+		assertEquals(new Point(5,8),car.getPosition());
+	}
+	
+	public void testMoveWithSouth()  throws IllegalMoveException{
+		//Wir positionieren unser Auto auf (5,5) und richten es mit unserem Kompass nach Süden aus
+		car.setOrientation(Orientation.SOUTH);
+		car.setPosition(new Point(5,5));
+		
+		//Wir stehen auf (5,5) und fahren 3 Felder vorwärts
+		car.move(3);
+		//Wir sollten nun auf (5,8) stehen
+		assertEquals(new Point(5,8),car.getPosition());
+		
+		//Wir stehen auf (5,8) und fahren 6 Felder rückwärts
+		car.move(-6);
+		//Wir sollten nun auf (5,2) stehen
+		assertEquals(new Point(5,2),car.getPosition());
 		
 	}
 	
-	public void testMoveWithZero () {
-		boolean thrownException = false;
+	public void testMoveWithWest()  throws IllegalMoveException{
+		//Wir positionieren unser Auto auf (5,5) und richten es mit unserem Kompass zu den alten Bundesländern aus
+		car.setOrientation(Orientation.WEST);
+		car.setPosition(new Point(5,5));
+		
+		//Wir stehen auf (5,5) und fahren 3 Felder vorwärts
+		car.move(3);
+		//Wir sollten nun auf (2,5) stehen
+		assertEquals(new Point(2,5),car.getPosition());
+		
+		//Wir stehen auf (2,5) und fahren 6 Felder rückwärts
+		car.move(-6);
+		//Wir sollten nun auf (8,5) stehen
+		assertEquals(new Point(8,5),car.getPosition());
+		
+	}
+	
+	public void testMoveWithEast()  throws IllegalMoveException{
+		//Wir positionieren unser Auto auf (5,5), es wurde offensichtlich gestohlen und ist Richtung Polen ausgerichtet
+		car.setOrientation(Orientation.EAST);
+		car.setPosition(new Point(5,5));
+		
+		//Wir stehen auf (5,5) und fahren 3 Felder vorwärts
+		car.move(3);
+		//Wir sollten nun auf (8,5) stehen
+		assertEquals(new Point(8,5),car.getPosition());
+		
+		//Wir stehen auf (8,5) und fahren 6 Felder rückwärts
+		car.move(-6);
+		//Wir sollten nun auf (2,5) stehen
+		assertEquals(new Point(2,5),car.getPosition());
+		
+	}
+	
+	public void testMoveWithZero () throws IllegalMoveException {
+		thrownException = false;
+		
+		//Wir positionieren unser Auto auf (5,5) und richten es mit unserem Kompass nach Norden aus
+		car.setOrientation(Orientation.NORTH);
+		car.setPosition(new Point(5,5));
+		
 		try {
 			car.move(0);
 		}
-		catch (IllegalArgumentException e) {
+		catch (IllegalMoveException e) {
 			thrownException = true;
 		}
 		assertTrue(thrownException);
