153131254bd005b874e1884376b9f0c3eeae54f7
tobias.rumrich@gmail.com <tobias.rumrich@gmail.com@3ea0aeef-910e-5cde-5b2f-4d9b01b7df46>
2010-12-02 16:55:00 +0000

    Changed Game to GameState. This highest entidity is now GamBoard. The time is stored in a Timer instance that is located in the controller

--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/GameStorage.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/GameStorage.java
@@ -2,11 +2,8 @@ package com.googlecode.waruma.rushhour.framework;
 public class GameStorage {
 
         
-  public Game loadGame(String location) {
+  public GameBoard loadGameBoard(String location) {
   return null;
-  }
-
-  public void saveGame(Game game, String location) {
   }
 
   public void saveGameBoard(GameBoard gameBoard, String location) {
