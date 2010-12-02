4954b953958a9d26e41e4573346074f824a026c5
mail2fabi@gmail.com <mail2fabi@gmail.com@3ea0aeef-910e-5cde-5b2f-4d9b01b7df46>
2010-12-02 13:52:53 +0000

    Bezeichnung angeglichen.

--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/IGameWonSubject.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/IGameWonSubject.java
@@ -2,5 +2,5 @@ package com.googlecode.waruma.rushhour.framework;
 
 public interface IGameWonSubject {
 	
-	public void registerGameWon (IGameWonObserver eventTrigger);
+	public void registerGameWon (IGameWonObserver eventTarget);
 }
