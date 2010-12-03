121d009e00a68b603ca613ba3ee92720a352d759
mail2fabi@gmail.com <mail2fabi@gmail.com@3ea0aeef-910e-5cde-5b2f-4d9b01b7df46>
2010-12-03 21:39:52 +0000

    GameState implemenitert

--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/GameState.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/GameState.java
@@ -1,18 +1,42 @@
 package com.googlecode.waruma.rushhour.framework;
 
-import java.util.Date;
 import java.util.Set;
 
 public class GameState implements IReachedDestinationObserver, IGameWonSubject{
-  private Set<IPlayer> player;
-
+  private Set<IGameWonObserver> observers;
+  private Set<IPlayer> players;
   
-  public void updateReachedDestination(IPlayer player) {
-	  
+  
+  //fügt einen zu überwachenden Player hinzu
+  public void addPlayer(IPlayer player) {
+	  players.add(player);
+	  player.registerReachedDestination(this);
   }
-  
+
+
+  //registriert einen GameWon-Observer
   public void registerGameWon(IGameWonObserver eventTrigger) {
-
-	
+	  observers.add(eventTrigger);
   }
+
+  //ReachedDestination Event wird behandelt
+  public void updateReachedDestination(IPlayer player) {
+	for (IPlayer currentPlayer : players) {
+		if(currentPlayer==player) {
+			players.remove(player);
+			checkWhetherLatest();
+		}	
+	}	
+  }
+  
+  //prüft, ob das ReachedDestiantion Event das letzte war und informiert in diesem Fall die GameWonObserver
+  public void checkWhetherLatest() {
+	  if(players.isEmpty())
+		  for (IGameWonObserver currentobserver : observers) {
+			  currentobserver.updateGameWon();
+		  }
+  }
+  
+  
+
 }
