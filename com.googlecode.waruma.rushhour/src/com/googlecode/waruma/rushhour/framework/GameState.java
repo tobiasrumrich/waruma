a1ac6f9032d7116d146bfb9f5ca346ca1892639f
mail2fabi@gmail.com <mail2fabi@gmail.com@3ea0aeef-910e-5cde-5b2f-4d9b01b7df46>
2010-12-03 21:42:41 +0000

    Mini-Änderung GameState

--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/GameState.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/GameState.java
@@ -2,6 +2,7 @@ package com.googlecode.waruma.rushhour.framework;
 
 import java.util.Set;
 
+
 public class GameState implements IReachedDestinationObserver, IGameWonSubject{
   private Set<IGameWonObserver> observers;
   private Set<IPlayer> players;
@@ -24,13 +25,13 @@ public class GameState implements IReachedDestinationObserver, IGameWonSubject{
 	for (IPlayer currentPlayer : players) {
 		if(currentPlayer==player) {
 			players.remove(player);
-			checkWhetherLatest();
+			checkWhetherLast();
 		}	
 	}	
   }
   
   //prüft, ob das ReachedDestiantion Event das letzte war und informiert in diesem Fall die GameWonObserver
-  public void checkWhetherLatest() {
+  public void checkWhetherLast() {
 	  if(players.isEmpty())
 		  for (IGameWonObserver currentobserver : observers) {
 			  currentobserver.updateGameWon();
