9906a4521afef313e73d9a2c780e6b2a9fe595f6
mail2fabi@gmail.com <mail2fabi@gmail.com@3ea0aeef-910e-5cde-5b2f-4d9b01b7df46>
2010-12-04 17:00:27 +0000

    CreationController

--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/game/RushHourBoardCreationController.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/game/RushHourBoardCreationController.java
@@ -2,27 +2,82 @@ package com.googlecode.waruma.rushhour.game;
 
 import java.awt.Point;
 
+import com.googlecode.waruma.rushhour.exceptions.IllegalBoardPositionException;
 import com.googlecode.waruma.rushhour.framework.GameBoard;
 import com.googlecode.waruma.rushhour.framework.Orientation;
 
+
+/**
+ * 
+ * @author fabian
+ * 
+ */
+
 public class RushHourBoardCreationController {
+	
+	
+	
 
 	private GameBoard gameBoard;
 
-	public void createCar(Point position, Enum<Orientation> orientation,
+	public void createCar(Point position, Orientation orientation,
 			Boolean steeringLock) {
+		
+		if (steeringLock==false){
+			StandardCar car = new StandardCar(new Boolean[][] {{true, true}}, position, orientation);
+			try {
+				gameBoard.addGameBoardObject(car);
+			} catch (IllegalBoardPositionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			SteeringLock car = new SteeringLock(new StandardCar(new Boolean[][] {{true, true}}, position, orientation));
+			try {
+				gameBoard.addGameBoardObject(car);
+			} catch (IllegalBoardPositionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();	
+			}
+		}
 	}
 
-	public void createPlayerCar(Point position, Point destinaton,
-			Enum<Orientation> orientation) {
-	}
 
-	public void createTruck(Point position, Enum<Orientation> orientation,
+	public void createTruck(Point position, Orientation orientation,
 			Boolean steeringLock) {
+		
+		if (steeringLock==false){
+			StandardCar car = new StandardCar(new Boolean[][] {{true, true, true}}, position, orientation);
+			try {
+				gameBoard.addGameBoardObject(car);
+			} catch (IllegalBoardPositionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			SteeringLock car = new SteeringLock(new StandardCar(new Boolean[][] {{true, true, true}}, position, orientation));
+			try {
+				gameBoard.addGameBoardObject(car);
+			} catch (IllegalBoardPositionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();	
+			}
+		}
+	}
+	
+	
+	public void createPlayerCar(Point position, Point destination,
+			Orientation orientation) {
+		PlayerCar car = new PlayerCar(new Boolean[][] {{true, true}}, position, orientation);
+		car.setDestination(destination);
+		try {
+			gameBoard.addGameBoardObject(car);
+		} catch (IllegalBoardPositionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();	
+		}
 	}
 
-	public void getGameBoard() {
-	}
 
 	public void saveGameBoard(String location) {
 	}
