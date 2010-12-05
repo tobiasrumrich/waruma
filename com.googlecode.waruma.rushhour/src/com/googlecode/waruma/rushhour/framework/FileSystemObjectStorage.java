9a501e987e7c7a6d4388b2fc20f1cad29d37461e
tobias.rumrich@gmail.com <tobias.rumrich@gmail.com@3ea0aeef-910e-5cde-5b2f-4d9b01b7df46>
2010-12-05 11:53:02 +0000

    FileSystemStorage

--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/FileSystemObjectStorage.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/FileSystemObjectStorage.java
@@ -1,12 +1,45 @@
 package com.googlecode.waruma.rushhour.framework;
 
-public class FileSystemObjectStorage implements IObjectStorage {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-	public static GameBoard loadGameBoard(String location) {
-		return null;
+public class FileSystemObjectStorage implements IObjectStorage {
+	
+	@Override
+	public void serialize(Serializable serializableObject, String location) throws IOException {
+
+		if (location == null || location.equals("")) {
+			throw new IllegalArgumentException("Illegal filename");
+		}
+		
+		//vgl. http://java.sun.com/developer/technicalArticles/Programming/serialization/
+		
+		FileOutputStream fileOutput = new FileOutputStream(location);
+		ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput);
+		objectOutput.writeObject(serializableObject);
+		objectOutput.close();
+		fileOutput.close();
 	}
 
-	public static void saveGameBoard(GameBoard gameBoard, String location) {
+	@Override
+	public Serializable deserialize(String location) throws IOException, ClassNotFoundException {
+		if (location == null || location.equals("")) {
+			throw new IllegalArgumentException("Illegal filename");
+		}
+		
+		//vgl. http://java.sun.com/developer/technicalArticles/Programming/serialization/
+		
+		FileInputStream fileOutput = new FileInputStream(location);
+		ObjectInputStream objectInput = new ObjectInputStream(fileOutput);
+		Serializable readObject = (Serializable) objectInput.readObject();
+		objectInput.close();
+		fileOutput.close();
+		
+		return readObject;
 	}
 
 }
