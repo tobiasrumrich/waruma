9af2e358ec95f5c50e51ccb9924fef75b2037746
tobias.rumrich@gmail.com <tobias.rumrich@gmail.com@3ea0aeef-910e-5cde-5b2f-4d9b01b7df46>
2010-12-05 11:53:02 +0000

    FileSystemStorage

--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/IObjectStorage.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/IObjectStorage.java
@@ -1,5 +1,18 @@
 package com.googlecode.waruma.rushhour.framework;
 
-public interface IObjectStorage {
+import java.io.IOException;
+import java.io.Serializable;
 
+/**
+ * 
+ * @author dep18237
+ *
+ */
+public interface IObjectStorage {
+	/**
+	 * Speichert ein Objekt an den über location übergebenen einen Speicherort.
+	 * @param location ist der Speicherort(Filesystem, Database, FTP, WebDAV,...)
+	 */
+	public Object deserialize(String location) throws IOException, ClassNotFoundException;
+	void serialize(Serializable object, String location) throws IOException;
 }
