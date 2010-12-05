4a2677bfa7c2373ad41206f11de0d9080d31a502
tobias.rumrich@gmail.com <tobias.rumrich@gmail.com@3ea0aeef-910e-5cde-5b2f-4d9b01b7df46>
2010-12-05 11:52:30 +0000

    BoardCreationController implemented

--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/game/RushHourBoardCreationController.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/game/RushHourBoardCreationController.java
@@ -1,89 +1,70 @@
 package com.googlecode.waruma.rushhour.game;
 
 import java.awt.Point;
-
 import com.googlecode.waruma.rushhour.exceptions.IllegalBoardPositionException;
 import com.googlecode.waruma.rushhour.framework.GameBoard;
+import com.googlecode.waruma.rushhour.framework.IObjectStorage;
 import com.googlecode.waruma.rushhour.framework.Orientation;
-
 
 /**
  * 
- * @author fabian
+ * @author fabian & tobias
  * 
  */
 
 public class RushHourBoardCreationController {
-	
-	
+
 	public RushHourBoardCreationController() {
 		super();
 	}
-
 
 	private GameBoard gameBoard;
 	private RushHourCollisionDetector collisionDetector;
 
 	public void createCar(Point position, Orientation orientation,
-			Boolean steeringLock) {
-		
-		if (steeringLock==false){
-			StandardCar car = new StandardCar(new Boolean[][] {{true, true}}, position, orientation);
-			try {
-				gameBoard.addGameBoardObject(car);
-			} catch (IllegalBoardPositionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			Boolean steeringLock) throws IllegalBoardPositionException {
+
+		if (steeringLock == false) {
+			StandardCar car = new StandardCar(
+					new Boolean[][] { { true, true } }, position, orientation);
+			gameBoard.addGameBoardObject(car);
 		} else {
-			SteeringLock car = new SteeringLock(new StandardCar(new Boolean[][] {{true, true}}, position, orientation));
-			try {
-				gameBoard.addGameBoardObject(car);
-			} catch (IllegalBoardPositionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();	
-			}
+			SteeringLock car = new SteeringLock(new StandardCar(
+					new Boolean[][] { { true, true } }, position, orientation));
+			gameBoard.addGameBoardObject(car);
 		}
 	}
-
 
 	public void createTruck(Point position, Orientation orientation,
-			Boolean steeringLock) {
-		
-		if (steeringLock==false){
-			StandardCar car = new StandardCar(new Boolean[][] {{true, true, true}}, position, orientation);
-			try {
-				gameBoard.addGameBoardObject(car);
-			} catch (IllegalBoardPositionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} else {
-			SteeringLock car = new SteeringLock(new StandardCar(new Boolean[][] {{true, true, true}}, position, orientation));
-			try {
-				gameBoard.addGameBoardObject(car);
-			} catch (IllegalBoardPositionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();	
-			}
-		}
-	}
-	
-	
-	public void createPlayerCar(Point position, Point destination,
-			Orientation orientation) {
-		PlayerCar car = new PlayerCar(new Boolean[][] {{true, true}}, position, orientation, collisionDetector);
-		car.setDestination(destination);
-		try {
+			Boolean steeringLock) throws IllegalBoardPositionException {
+
+		if (!steeringLock) {
+			StandardCar car = new StandardCar(new Boolean[][] { { true, true,
+					true } }, position, orientation);
 			gameBoard.addGameBoardObject(car);
-		} catch (IllegalBoardPositionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();	
+		} else {
+			SteeringLock car = new SteeringLock(new StandardCar(
+					new Boolean[][] { { true, true, true } }, position,
+					orientation));
+			gameBoard.addGameBoardObject(car);
 		}
 	}
 
+	public void createPlayerCar(Point position, Point destination,
+			Orientation orientation) throws IllegalBoardPositionException {
+		PlayerCar car = new PlayerCar(new Boolean[][] { { true, true } },
+				position, orientation, collisionDetector);
+		car.setDestination(destination);
 
-	public void saveGameBoard(String location) {
+		// throws IllegalBoardPositionException
+		gameBoard.addGameBoardObject(car);
+	}
+
+	public void loadGameBoard(IObjectStorage objectStorage, String location) {
+		objectStorage.deserialize(location);
+	}
+	public void saveGameBoard(IObjectStorage objectStorage, String location) {
+		objectStorage.serialize(location);
 	}
 
 }
