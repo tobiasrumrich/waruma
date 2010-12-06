f8bd7151d82840161f7dfb2dfd65153bd5d52456
tobias.rumrich@gmail.com <tobias.rumrich@gmail.com@3ea0aeef-910e-5cde-5b2f-4d9b01b7df46>
2010-12-06 22:25:34 +0000

    An UI gearbeitet ... Felder können jetzt rückmelden, wenn sie von der Maus überfahren werden
    Mit dem LayoutManager haut das noch nicht so hin (schritte erstmal wieder entfernt)

--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/ui/AbstractGameBoardWidget.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/ui/AbstractGameBoardWidget.java
@@ -1,13 +1,15 @@
 package com.googlecode.waruma.rushhour.ui;
 
-import java.util.ArrayList;
-
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
-import com.swtdesigner.SWTResourceManager;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseTrackListener;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+import com.swtdesigner.SWTResourceManager;
 
 public class AbstractGameBoardWidget extends Composite {
 
@@ -16,56 +18,104 @@ public class AbstractGameBoardWidget extends Composite {
 	private int breite;
 	private int minHoeheFeld = 50;
 	private int minBreiteFeld = 50;
+	private String inField = "NULL!!";
+
+	private class FieldMouseListener implements MouseTrackListener {
+		
+		private int fieldX;
+		private int fieldY;
+
+		public FieldMouseListener(int fieldX, int fieldY) {
+			this.fieldX = fieldX;
+			this.fieldY = fieldY;
+		}
+		@Override
+		public void mouseEnter(MouseEvent arg0) {
+			inField = fieldX + "--" + fieldY;
+			System.out.println(fieldX + " -- " + fieldY);
+		}
+
+		@Override
+		public void mouseExit(MouseEvent arg0) {
+			inField = "Tschüssing";
+
+		}
+
+		@Override
+		public void mouseHover(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+
+		}
+	}
 
 	/**
 	 * Create the composite.
+	 * 
 	 * @param parent
 	 * @param style
-	 * @param breite 
-	 * @param hoehe 
+	 * @param breite
+	 * @param hoehe
 	 */
-	public AbstractGameBoardWidget(Composite parent, int style, int breite, int hoehe) {
+	public AbstractGameBoardWidget(Composite parent, int style, int breite,
+			int hoehe) {
 		super(parent, style);
-		
+
 		this.hoehe = hoehe;
 		this.breite = breite;
-		System.out.println(breite+"++++++");
+
 		spielbrett = new Label[hoehe][breite];
 		setLayout(new GridLayout(breite, false));
 		((GridLayout) this.getLayout()).horizontalSpacing = 5;
 		((GridLayout) this.getLayout()).verticalSpacing = 5;
-		
-	
-				
-		
-		
-		
+
 		for (int i = 0; i < hoehe; i++) {
 			for (int j = 0; j < breite; j++) {
 				spielbrett[i][j] = new Label(this, SWT.NONE);
-				spielbrett[i][j].setText(""+i + "" + j);
-				spielbrett[i][j].setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-				
-				if ((i+j) % 2 == 0) {
-					spielbrett[i][j].setBackground(SWTResourceManager.getColor(166, 202, 240));	
+				spielbrett[i][j].setText("" + i + "" + j);
+				spielbrett[i][j].setLayoutData(new GridData(SWT.FILL, SWT.FILL,
+						true, true, 1, 1));
+
+				if ((i + j) % 2 == 0) {
+					spielbrett[i][j].setBackground(SWTResourceManager.getColor(
+							166, 202, 240));
 				} else {
-					spielbrett[i][j].setBackground(SWTResourceManager.getColor(119, 136, 153));	
+					spielbrett[i][j].setBackground(SWTResourceManager.getColor(
+							119, 136, 153));
 				}
+				spielbrett[i][j]
+						.addMouseTrackListener(new FieldMouseListener(i,j));
 			}
-			
+
 		}
 	}
-	
-	
-	public int getMinHeight (){
-		return (hoehe * minHoeheFeld)+((hoehe-1)*((GridLayout) this.getLayout()).horizontalSpacing);
+
+	public int getMinHeight() {
+		return (hoehe * minHoeheFeld)
+				+ ((hoehe - 1) * ((GridLayout) this.getLayout()).horizontalSpacing);
 	}
-	
-	public int getMinWidth (){
-		return (breite*minHoeheFeld) + ((breite-1)*((GridLayout) this.getLayout()).verticalSpacing);
+
+	public int getMinWidth() {
+		return (breite * minHoeheFeld)
+				+ ((breite - 1) * ((GridLayout) this.getLayout()).verticalSpacing);
 	}
-	
-	
+
+	public Point getCurrentFieldSize() {
+		return (spielbrett[0][0].getSize());
+	}
+
+	public String woBinIch() {
+		/*
+		 * int aktuelleBreite = spielbrett[0][0].getSize().x; int aktuelleHoehe
+		 * = spielbrett[0][0].getSize().y; int aktuellerAbstandX = ((GridLayout)
+		 * this.getLayout()).horizontalSpacing; int aktuellerAbstandY =
+		 * ((GridLayout) this.getLayout()).verticalSpacing;
+		 * 
+		 * int pixelFeldX = point.x /
+		 * (breite*(aktuelleBreite+aktuellerAbstandX)); int pixelFeldY = point.y
+		 * / (hoehe+aktuellerAbstandY);
+		 */
+		return inField;
+	}
 
 	@Override
 	protected void checkSubclass() {
