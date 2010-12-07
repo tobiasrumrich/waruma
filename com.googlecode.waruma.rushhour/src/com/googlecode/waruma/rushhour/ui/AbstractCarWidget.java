4e072f0c420068eb268b7e2d794d0f13e5feadf2
tobias.rumrich@gmail.com <tobias.rumrich@gmail.com@3ea0aeef-910e-5cde-5b2f-4d9b01b7df46>
2010-12-07 23:07:42 +0000

    Auto ist jetzt in Spielfeld gefangen (Zuordnung ist noch versetzt aber funktioniert ansonsten)

--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/ui/AbstractCarWidget.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/ui/AbstractCarWidget.java
@@ -1,5 +1,7 @@
 package com.googlecode.waruma.rushhour.ui;
 
+import java.util.HashMap;
+
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
@@ -7,8 +9,14 @@ import com.swtdesigner.SWTResourceManager;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
 
 public class AbstractCarWidget extends Composite {
+
+	private Image image;
 
 	/**
 	 * Create the composite.
@@ -18,23 +26,52 @@ public class AbstractCarWidget extends Composite {
 	 */
 	public AbstractCarWidget(Composite parent, int style) {
 		super(parent, SWT.EMBEDDED);
+		addControlListener(new ControlAdapter() {
+			@Override
+			public void controlResized(ControlEvent e) {
+				setBackgroundImage(resizeImage(new Point(getBounds().width,getBounds().height)));
+			}
+		});
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseUp(MouseEvent e) {
 				System.out.println("123412341234");
 			}
 		});
-		Image image = SWTResourceManager.getImage(AbstractCarWidget.class,
-		"/com/googlecode/waruma/rushhour/ui/images/CAR_blau.png");
-		
-		this.setBounds(image.getBounds().x,image.getBounds().y,image.getBounds().width,image.getBounds().height);
+
+		image = SWTResourceManager.getImage(AbstractCarWidget.class,
+				"/com/googlecode/waruma/rushhour/ui/images/CAR_blau.png");
+
+		//ImageData imgData = image.getImageData();
+		//imgData = imgData.scaledTo(400, 300);
+		//image = new Image(this.getDisplay(), imgData);
+
+		this.setBounds(image.getBounds().x, image.getBounds().y,
+				image.getBounds().width, image.getBounds().height);
 		this.setBackgroundImage(image);
 		setLayout(null);
-		System.out.println("X=" + image.getBounds().height + ";Y=" + image.getBounds().width);
+		System.out.println("X=" + image.getBounds().height + ";Y="
+				+ image.getBounds().width);
+
+	}
+
+	private Image resizeImage(Point newSize) {
+		HashMap<Point, Image> image_cache = new HashMap<Point, Image>();
+
+		if (image_cache.containsKey(newSize)) {
+			return image_cache.get(newSize);
+		}
+
+		ImageData imgData = image.getImageData();
+
+		imgData = imgData.scaledTo(newSize.x, newSize.y);
+		image = new Image(this.getDisplay(), imgData);
+
+		image_cache.put(newSize, image);
+		return image;
 		
-		
-		//this.setBounds(200, 100, 200, 100);
-		
+		//this.setBounds(image.getBounds().x, image.getBounds().y,
+		//		image.getBounds().width, image.getBounds().height);
 
 	}
 
