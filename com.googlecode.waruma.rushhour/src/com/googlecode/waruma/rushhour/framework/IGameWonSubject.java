d31169c4d44c7b7a11a37edda8de421f846bca1c
mail2fabi@gmail.com <mail2fabi@gmail.com@3ea0aeef-910e-5cde-5b2f-4d9b01b7df46>
2010-12-16 12:24:40 +0000

    comments

--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/IGameWonSubject.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/IGameWonSubject.java
@@ -2,5 +2,10 @@ package com.googlecode.waruma.rushhour.framework;
 
 public interface IGameWonSubject {
 
+	/**
+	 * registriert eine IGameWonObserver
+	 * 
+	 * @param eventTarget Observer
+	 */
 	public void registerGameWon(IGameWonObserver eventTarget);
 }
--- com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/IReachedDestinationObserver.java
+++ com.googlecode.waruma.rushhour/src/com/googlecode/waruma/rushhour/framework/IReachedDestinationObserver.java
@@ -2,5 +2,11 @@ package com.googlecode.waruma.rushhour.framework;
 
 public interface IReachedDestinationObserver {
 
+	/**
+	 * teilt dem Observer mit, dass der IPlayer das Ziel erreicht hat
+	 * 
+	 * @param player
+	 *            IPlayer-Objekt, welches das Ziel erreicht hat
+	 */
 	public void updateReachedDestination(IPlayer player);
 }
